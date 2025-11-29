64f58d1cfdeff8eb3a6e49c0e39d3d04a16e7ea6
Aravindh V <aravindh.vellaiyan@gmail.com>
2025-11-29 22:59:05 +0530

    Base Application with dependencies

---
+++ src/main/java/com/cricforge/team_management/CricketTeamManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.cricforge.team_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CricketTeamManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CricketTeamManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cricforge/team_management/CricketTeamManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cricforge.team_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CricketTeamManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
